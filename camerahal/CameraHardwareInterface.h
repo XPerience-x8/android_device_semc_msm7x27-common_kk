e7218d45d4f256f7995bb6b1f83e8e615ef370b7
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-03-10 18:45:40 -0600

    Updates and new aditions like libnl,iw,recovery fixes
    Change-Id: I8d73950f0aba8f8e74687564982c4f95638640bd

--- camerahal/CameraHardwareInterface.h
+++ camerahal/CameraHardwareInterface.h
@@ -20,7 +20,8 @@
 
 #include <binder/IMemory.h>
 #include <utils/RefBase.h>
-#include <gui/ISurface.h>
+//#include <gui/ISurface.h>
+#include <gui/ISurfaceComposer.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
 
